da9f2bff1ee3e2f428b34b6a685314e8374c9377
Aga Magiera <aga.magiera@fanduel.com>
2018-12-06 15:30:09 +0000

    Created a MyWorker JAVA file with a function that multiplies a given value

---
+++ app/src/main/java/com/example/agamagiera/firstapp/MyWorker.java
@@ -0,0 +1,11 @@
+package com.example.agamagiera.firstapp;
+
+/**
+ * Created by Aga
+ */
+public class MyWorker {
+
+    public static int doubleTheValue(int value) {
+        return value * 2;
+    }
+}
